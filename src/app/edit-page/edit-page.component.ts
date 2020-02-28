82a3d2729952e4bf6c47581a93c0dccca88315f3
ibulygin <bit19941@gmail.com>
2020-02-28 19:35:01 +0300

    location

--- src/app/edit-page/edit-page.component.ts
+++ src/app/edit-page/edit-page.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { EditSkillService } from '../services/editSkil/editSkill.service';
 import { SkillIdService } from '../services/skillId/skillId.service';
+import {Location} from '@angular/common';
 
 @Component({
   selector: 'app-edit-page',
@@ -9,18 +10,24 @@ import { SkillIdService } from '../services/skillId/skillId.service';
 })
 export class EditPageComponent implements OnInit {
   skill: any;
+  location: Location;
 
   constructor(
     private editSkillService: EditSkillService,
-    private skillIdService: SkillIdService
-    ) { }
+    private skillIdService: SkillIdService,
+    location: Location
+    ) { 
+      this.location = location;
+    }
 
   ngOnInit() {
     this.editSkillService.getSkillById(this.skillIdService.get()).subscribe((skill) => this.skill = skill );
+
   }
 
   get() {
     console.log(this.skill);
+    console.log(this.location.path())
   }
 
 }
